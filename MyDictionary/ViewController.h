31e840fc8605072b3fcb92aac79b3725d1cd2c1a
charlie <chenli19880824@gmail.com>
2017-12-01 10:08:07 +0800

    Initial Commit

---
+++ MyDictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDictionary
+//
+//  Created by chenli on 01/12/2017.
+//  Copyright © 2017 chenli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyDictionary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDictionary
+//
+//  Created by chenli on 01/12/2017.
+//  Copyright © 2017 chenli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
